f505c9a2006fbbcd408c9f639c43f29f85a49300
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-09 11:35:37 -0600

    Formato Base

---
+++ src/armus/lib/scanner/Scanner.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package armus.lib.scanner;
+
+/**
+ *
+ * @author root
+ */
+public class Scanner {
+    static {
+        System.load("/home/nextor/NetBeansProjects/Armus.Lib/dist/libArmus.Lib.so");
+    }
+    public native String[] lsFiles(String pathFirstFile);
+}
