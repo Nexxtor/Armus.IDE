6d5ec8cf288080f6afe1f7e8a125187f123092bc
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-16 16:44:25 -0600

    Ordenamiento del código fuente y eliminación de comentarios por defecto.

--- src/armus/ide/Main.java
+++ src/armus/ide/Main.java
@@ -1,21 +1,12 @@
-
 package armus.ide;
 
 import armus.lib.scanner.Scanner;
 
-/**
- *
- * @author root
- */
 public class Main {
 
-    
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner a  = new Scanner();
-        a.lsFiles("/home/nextor/Escritorio/cualquiercosa.acl");
+        a.lsFiles("/home/nextor/Escritorio/proyecto/main.acl");
     }
     
 }
