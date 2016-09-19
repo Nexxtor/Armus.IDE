2fb01b6573915ad2fd770b5234a87ca9ebb0885c
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-19 15:03:18 -0600

    redefinida de lsTokens

--- src/armus/lib/scanner/Scanner.java
+++ src/armus/lib/scanner/Scanner.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package armus.lib.scanner;
 
-/**
- *
- * @author root
- */
 public class Scanner {
     static {
         System.load("/home/nextor/NetBeansProjects/Armus.Lib/dist/libArmus.Lib.so");
     }
     public native String[] lsFiles(String pathFirstFile);
-    public native String[] lsTokens(String pathSecondFile);
+    public native String[] lsTokens(String[] allFiles);
 }
