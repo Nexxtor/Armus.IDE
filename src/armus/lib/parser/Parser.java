091d56143e6e89bf51053e4753383dad3aeb8c50
michel725 <michel.725@outlook.com>
2016-11-25 23:35:56 -0600

    Eliminación de espacios en el código

--- src/armus/lib/parser/Parser.java
+++ src/armus/lib/parser/Parser.java
@@ -1,25 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package armus.lib.parser;
 
-/**
- *
- * @author nextor
- */
 public class Parser {
     static {
         System.load("/opt/armus/lib/libArmus.Lib.so");
     }
     
     /**
-     *  Analiza la sintaxis de un proyecto o archivo en lenguaje 
-     *  armus
+     * Analiza la sintaxis de un proyecto o archivo en lenguaje Armus
      * @param lsFiles lista de archivos que se desea analizar la sintaxis
      * @return null si no exite ni un error, String[] Si posee errores.  
-     * 
      */
     public native String[] run(String[] lsFiles);
 }
