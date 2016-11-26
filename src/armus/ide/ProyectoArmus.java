5a9956fb7c833ca089bfaa036c03715d14948398
michel725 <michel.725@outlook.com>
2016-11-25 20:46:58 -0600

    Añadido el cambio de apariencia, para adaptarlo al S.O. actual

--- src/armus/ide/ProyectoArmus.java
+++ src/armus/ide/ProyectoArmus.java
@@ -1,30 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package armus.ide;
 
 import armus.ide.gui.IDE_Armus;
 import armus.lib.scanner.Scanner;
 import armus.lib.parser.Parser;
 import javax.swing.ImageIcon;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-/**
- *
- * @author stefa_000
- */
 public class ProyectoArmus {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-       IDE_Armus ventana = new IDE_Armus();
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        //Cambiar apariencia al sistema operativo actual
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            System.out.println(e.getMessage());
+        } //Fin cambiar apariencia
+
+        IDE_Armus ventana = new IDE_Armus();
         ventana.setVisible(true);
-      
-
-
     }
-
 }
